b9ff06c9428401585e6f14a0f3f2ebd63c90a8ad
Rushikesh Kadlag <62883177+rushikadlag0911@users.noreply.github.com>
2021-07-15 15:35:31 +0530

    ReactJS First App

---
+++ src/components/counter.jsx
@@ -0,0 +1,56 @@
+import { thisExpression } from '@babel/types';
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        value: this.props.value
+    };
+
+  //  constructor() { // instead of using this we can use arrow functions
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+    handleIncrement = product => {
+        console.log(product);
+        this.setState({count: this.state.count + 1 });
+    }
+
+    doHandleIncrement = () => {
+        this.handleIncrement({ id: 1});
+    };
+
+    render() {
+
+       
+
+        return ( 
+         <div>
+            
+            <span className={this.getBadgeClasses()}>
+                {this.formatCount()}
+            </span>
+
+            <button 
+                onClick={() => this.handleIncrement({id : 1}) }
+                className="btn btn-secondary btn-sm">Increment
+            </button>
+        
+         </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { value } = this.state;
+        
+        return value === 0 ? "Zero" : value;
+    }
+}
+ 
+export default Counter;
